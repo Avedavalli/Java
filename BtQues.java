fc345f38f42718f4cbcc493f799ea93139e7725f
Avedavalli <45105944+Avedavalli@users.noreply.github.com>
2023-11-06 16:59:27 +0530

    Create BtQues.java

---
+++ BtQues.java
@@ -0,0 +1,35 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class BtQues {
+//    List<Integer> l=new ArrayList<>();
+//    List<Integer> listprime=new ArrayList<>();
+//    List<Integer> primes=new ArrayList<>();
+//    boolean present=false;
+//    for(int i=0;i<l.length();i++){
+//        for(int j=2;j<i/2;j++){
+//            if(l[i]/j==0){
+//                present=true;
+//            }
+//        }
+//        if(!present){
+//            listprime.add(l[i]);
+//        }
+//    }
+//     for(int i=2;i<100;i++){
+//        for(int j=2;j<i/2;j++){
+//            if(i/j==0){
+//                present=true;
+//            }
+//        }
+//        if(!present){
+//            primes.add(l[i]);
+//        }
+//    }
+//     for(int i=0;i<listprimes.length;i++){
+//
+//    }
+//    //l.stream().filter(x->x)
+//    //list.stream().collectors.grounpingBy(emp->emp.getJjoinibdate().getMonth(),collectors.counting())
+//}
