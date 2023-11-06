a2ca4517f1ad01d7832acaeaeb72602284f7fa2a
Avedavalli <45105944+Avedavalli@users.noreply.github.com>
2023-11-06 17:08:13 +0530

    Create Mutable.java

---
+++ Mutable.java
@@ -0,0 +1,45 @@
+// public class Mutable extends Immutable {
+//  private int rvalue;
+//  private String name;
+
+//    public Mutable(int val, String name) {
+//        ///super(val, name);
+//    }
+
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+// //    public Mutable( int rvalue, String name1) {
+// //
+// //
+// //
+// //        this.rvalue = rvalue;
+// //        this.name = name1;
+// //
+// //    }
+
+
+//    public int getRvalue() {
+//        return rvalue;
+//    }
+
+//    public void setRvalue(int rvalue) {
+//        this.rvalue = rvalue;
+//    }
+
+//    public static void main(String[] args) {
+//        int val=4;
+//        String name="veda";
+//        Mutable obj=new Mutable(val,name);
+//        Immutable immobj=(Immutable)obj;
+//        System.out.println(immobj.getValue());
+//       // obj.setRvalue(10);
+//        System.out.println(immobj.getValue());
+//        val=23;
+//        name="valli";
+//        System.out.println(immobj.getValue()+" "+immobj.getName());
+//        System.out.println(obj.getRvalue()+" "+obj.getName());
+//    }
+// }
